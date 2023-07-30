ececd72fbfcf9355ba7e2af88be9f2dc03ff5e21
TM <tm@oxenfree.net>
2023-07-30 18:50:02 +0700

    prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    alpinejs(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), alpinejs(), prefetch()]
 });
